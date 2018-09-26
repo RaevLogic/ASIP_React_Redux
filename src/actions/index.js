931039a21ef5629cfce48e95a4b690f3caf249e2
Roger K <raevlogic@gmail.com>
2018-09-26 16:28:18 -0700

    Upload App to Github

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '7b2d0966f2c0427fdfa92b6993251458';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us&units=imperial`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
