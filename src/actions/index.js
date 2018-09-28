f36fa6e4778ff9351b95abe61fdc11ed4360ecee
Roger K <raevlogic@gmail.com>
2018-09-27 18:53:50 -0700

    Fixing https error

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '7b2d0966f2c0427fdfa92b6993251458';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
